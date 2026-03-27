948ab8cce42c6d80ecfcd200e5e08861e198215d
christesh072-design <christesh072@gmail.com>
2026-03-27 12:44:22 +0300

    Create Footer component with contact information

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Contact Information:</p>
+      <p>Email: christesh494@gmail.com</p>
+      <p>Phone: +254757860569</p>
+      <p>Instagram: <a href="https://instagram.com/ynw_echo">ynw_echo</a></p>
+    </footer>
+  );
+};
+
+export default Footer;
